2882357854ffab5dc982ca66f6c74086d583f411
Narcosynthetic <whendream_dayunite@yahoo.com>
2016-11-15 23:20:05 +0200

    Minor Changes Sap Managment

--- OTE/frmSapsManagement.Designer.cs
+++ OTE/frmSapsManagement.Designer.cs
@@ -31,47 +31,40 @@
             this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmSapsManagement));
             this.sAPBindingSource = new System.Windows.Forms.BindingSource(this.components);
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.diaxeirhshExoplismouOTEDataSet = new OTE.DiaxeirhshExoplismouOTEDataSet();
-            this.getSapsDetailsBindingSource = new System.Windows.Forms.BindingSource(this.components);
-            this.getSapsDetailsTableAdapter = new OTE.DiaxeirhshExoplismouOTEDataSetTableAdapters.GetSapsDetailsTableAdapter();
+            this.dvgSap = new System.Windows.Forms.DataGridView();
             this.dataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.TyposDescr = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.getSapsDetailsBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.diaxeirhshExoplismouOTEDataSet = new OTE.DiaxeirhshExoplismouOTEDataSet();
+            this.getSapsDetailsTableAdapter = new OTE.DiaxeirhshExoplismouOTEDataSetTableAdapters.GetSapsDetailsTableAdapter();
+            this.txbSap = new System.Windows.Forms.TextBox();
+            this.txbCategory = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnAddSap = new System.Windows.Forms.Button();
+            this.btnUpdateSap = new System.Windows.Forms.Button();
+            this.btnDeleteSap = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.sAPBindingSource)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.diaxeirhshExoplismouOTEDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dvgSap)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.getSapsDetailsBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.diaxeirhshExoplismouOTEDataSet)).BeginInit();
             this.SuspendLayout();
             // 
-            // dataGridView1
+            // dvgSap
             // 
-            this.dataGridView1.AllowUserToAddRows = false;
-            this.dataGridView1.AllowUserToDeleteRows = false;
-            this.dataGridView1.AutoGenerateColumns = false;
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dvgSap.AllowUserToAddRows = false;
+            this.dvgSap.AllowUserToDeleteRows = false;
+            this.dvgSap.AutoGenerateColumns = false;
+            this.dvgSap.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dvgSap.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.dataGridViewTextBoxColumn1,
             this.TyposDescr});
-            this.dataGridView1.DataSource = this.getSapsDetailsBindingSource;
-            this.dataGridView1.Location = new System.Drawing.Point(112, 28);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.ReadOnly = true;
-            this.dataGridView1.Size = new System.Drawing.Size(466, 227);
-            this.dataGridView1.TabIndex = 0;
-            // 
-            // diaxeirhshExoplismouOTEDataSet
-            // 
-            this.diaxeirhshExoplismouOTEDataSet.DataSetName = "DiaxeirhshExoplismouOTEDataSet";
-            this.diaxeirhshExoplismouOTEDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
-            // 
-            // getSapsDetailsBindingSource
-            // 
-            this.getSapsDetailsBindingSource.DataMember = "GetSapsDetails";
-            this.getSapsDetailsBindingSource.DataSource = this.diaxeirhshExoplismouOTEDataSet;
-            // 
-            // getSapsDetailsTableAdapter
-            // 
-            this.getSapsDetailsTableAdapter.ClearBeforeFill = true;
+            this.dvgSap.DataSource = this.getSapsDetailsBindingSource;
+            this.dvgSap.Location = new System.Drawing.Point(112, 28);
+            this.dvgSap.Name = "dvgSap";
+            this.dvgSap.ReadOnly = true;
+            this.dvgSap.Size = new System.Drawing.Size(466, 227);
+            this.dvgSap.TabIndex = 0;
             // 
             // dataGridViewTextBoxColumn1
             // 
@@ -87,28 +80,112 @@
             this.TyposDescr.Name = "TyposDescr";
             this.TyposDescr.ReadOnly = true;
             // 
+            // getSapsDetailsBindingSource
+            // 
+            this.getSapsDetailsBindingSource.DataMember = "GetSapsDetails";
+            this.getSapsDetailsBindingSource.DataSource = this.diaxeirhshExoplismouOTEDataSet;
+            // 
+            // diaxeirhshExoplismouOTEDataSet
+            // 
+            this.diaxeirhshExoplismouOTEDataSet.DataSetName = "DiaxeirhshExoplismouOTEDataSet";
+            this.diaxeirhshExoplismouOTEDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // getSapsDetailsTableAdapter
+            // 
+            this.getSapsDetailsTableAdapter.ClearBeforeFill = true;
+            // 
+            // txbSap
+            // 
+            this.txbSap.Location = new System.Drawing.Point(111, 281);
+            this.txbSap.Name = "txbSap";
+            this.txbSap.Size = new System.Drawing.Size(100, 20);
+            this.txbSap.TabIndex = 1;
+            // 
+            // txbCategory
+            // 
+            this.txbCategory.Location = new System.Drawing.Point(220, 282);
+            this.txbCategory.Name = "txbCategory";
+            this.txbCategory.Size = new System.Drawing.Size(100, 20);
+            this.txbCategory.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(109, 266);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(28, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "SAP";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(217, 266);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Category";
+            // 
+            // btnAddSap
+            // 
+            this.btnAddSap.Location = new System.Drawing.Point(112, 308);
+            this.btnAddSap.Name = "btnAddSap";
+            this.btnAddSap.Size = new System.Drawing.Size(75, 23);
+            this.btnAddSap.TabIndex = 6;
+            this.btnAddSap.Text = "Add";
+            this.btnAddSap.UseVisualStyleBackColor = true;
+            this.btnAddSap.Click += new System.EventHandler(this.btnAddSap_Click);
+            // 
+            // btnUpdateSap
+            // 
+            this.btnUpdateSap.Location = new System.Drawing.Point(220, 308);
+            this.btnUpdateSap.Name = "btnUpdateSap";
+            this.btnUpdateSap.Size = new System.Drawing.Size(75, 23);
+            this.btnUpdateSap.TabIndex = 7;
+            this.btnUpdateSap.Text = "Update";
+            this.btnUpdateSap.UseVisualStyleBackColor = true;
+            this.btnUpdateSap.Click += new System.EventHandler(this.btnUpdateSap_Click);
+            // 
+            // btnDeleteSap
+            // 
+            this.btnDeleteSap.Location = new System.Drawing.Point(325, 307);
+            this.btnDeleteSap.Name = "btnDeleteSap";
+            this.btnDeleteSap.Size = new System.Drawing.Size(75, 23);
+            this.btnDeleteSap.TabIndex = 8;
+            this.btnDeleteSap.Text = "Delete";
+            this.btnDeleteSap.UseVisualStyleBackColor = true;
+            this.btnDeleteSap.Click += new System.EventHandler(this.btnDeleteSap_Click);
+            // 
             // frmSapsManagement
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(699, 420);
-            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnDeleteSap);
+            this.Controls.Add(this.btnUpdateSap);
+            this.Controls.Add(this.btnAddSap);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txbCategory);
+            this.Controls.Add(this.txbSap);
+            this.Controls.Add(this.dvgSap);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "frmSapsManagement";
             this.Text = "SAP\'s Management";
             this.Load += new System.EventHandler(this.SapsManagement_Load);
             ((System.ComponentModel.ISupportInitialize)(this.sAPBindingSource)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.diaxeirhshExoplismouOTEDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dvgSap)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.getSapsDetailsBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.diaxeirhshExoplismouOTEDataSet)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.BindingSource sAPBindingSource;
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dvgSap;
         private System.Windows.Forms.DataGridViewTextBoxColumn idDataGridViewTextBoxColumn;
         private System.Windows.Forms.DataGridViewTextBoxColumn sapDataGridViewTextBoxColumn;
         private System.Windows.Forms.DataGridViewTextBoxColumn typosMhxanhmatosIdDataGridViewTextBoxColumn;
@@ -117,5 +194,12 @@
         private DiaxeirhshExoplismouOTEDataSetTableAdapters.GetSapsDetailsTableAdapter getSapsDetailsTableAdapter;
         private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn1;
         private System.Windows.Forms.DataGridViewTextBoxColumn TyposDescr;
+        private System.Windows.Forms.TextBox txbSap;
+        private System.Windows.Forms.TextBox txbCategory;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnAddSap;
+        private System.Windows.Forms.Button btnUpdateSap;
+        private System.Windows.Forms.Button btnDeleteSap;
     }
 }
--- OTE/frmSapsManagement.cs
+++ OTE/frmSapsManagement.cs
@@ -81,8 +81,78 @@ namespace OTE
             return dt;
         }
 
+
         #endregion
 
-    }
 
+        private void btnAddSap_Click(object sender, EventArgs e)
+        {
+            InsertSapToDB();
+            PopulateGrid();
+        }
+
+        private void btnUpdateSap_Click(object sender, EventArgs e)
+        {
+
+            PopulateGrid();
+
+        }
+
+        private void btnDeleteSap_Click(object sender, EventArgs e)
+        {
+            DeleteSapToDB();
+            PopulateGrid();
+
+        }
+
+        private void InsertSapToDB()
+        {
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["Main"].ConnectionString))
+                {
+                    conn.Open();
+                    using (SqlCommand cmd = new SqlCommand("[dbo].[InsertSap]", conn))
+                    {
+                        cmd.CommandType = CommandType.StoredProcedure;
+                        cmd.Parameters.AddWithValue("@Sap", txbSap.Text.Trim());
+                        cmd.Parameters.AddWithValue("@TyposMhxanhmatos", txbCategory.Text.Trim());
+                        cmd.ExecuteNonQuery();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                string error = ex.Message;
+                throw;
+            }
+
+        }
+
+        private void DeleteSapToDB()
+        {          
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["Main"].ConnectionString))
+                {
+                    conn.Open();
+                    using (SqlCommand cmd = new SqlCommand("[dbo].[DeleteSap]", conn))
+                    {
+                        cmd.CommandType = CommandType.StoredProcedure;
+                        cmd.Parameters.AddWithValue("@Sap", txbSap.Text.Trim());
+                        cmd.Parameters.AddWithValue("@TyposMhxanhmatos", txbCategory.Text.Trim());
+                        cmd.ExecuteNonQuery();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                string error = ex.Message;
+                throw;
+            }
+
+        }
+                
+
+    }
 }
