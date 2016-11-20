25c0c979d14c961b613c762b3a0baf35d7558dbe
oobeeliix <liyeree@gmail.com>
2016-11-20 23:33:32 +0200

    frmHome missing files

---
+++ OTE/frmHome.Designer.cs
@@ -0,0 +1,281 @@
+﻿namespace OTE
+{
+    partial class frmHome
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmHome));
+            this.pnlTechnician = new System.Windows.Forms.Panel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblTechSatellites = new System.Windows.Forms.Label();
+            this.lblTechIptvs = new System.Windows.Forms.Label();
+            this.lblTechModems = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pnlSupervisor = new System.Windows.Forms.Panel();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.lblSuperSatellites = new System.Windows.Forms.Label();
+            this.lblSuperIptvs = new System.Windows.Forms.Label();
+            this.lblSuperModems = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.btnSetBackColor = new System.Windows.Forms.Button();
+            this.btnSetForeColor = new System.Windows.Forms.Button();
+            this.pnlTechnician.SuspendLayout();
+            this.pnlSupervisor.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnlTechnician
+            // 
+            this.pnlTechnician.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pnlTechnician.Controls.Add(this.label4);
+            this.pnlTechnician.Controls.Add(this.label3);
+            this.pnlTechnician.Controls.Add(this.label2);
+            this.pnlTechnician.Controls.Add(this.lblTechSatellites);
+            this.pnlTechnician.Controls.Add(this.lblTechIptvs);
+            this.pnlTechnician.Controls.Add(this.lblTechModems);
+            this.pnlTechnician.Controls.Add(this.label1);
+            this.pnlTechnician.Location = new System.Drawing.Point(12, 12);
+            this.pnlTechnician.Name = "pnlTechnician";
+            this.pnlTechnician.Size = new System.Drawing.Size(130, 127);
+            this.pnlTechnician.TabIndex = 0;
+            this.pnlTechnician.Visible = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(52, 100);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(49, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Satellites";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(52, 76);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "IP TV\'s";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(52, 52);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Modems";
+            // 
+            // lblTechSatellites
+            // 
+            this.lblTechSatellites.AutoSize = true;
+            this.lblTechSatellites.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.lblTechSatellites.Location = new System.Drawing.Point(17, 100);
+            this.lblTechSatellites.Name = "lblTechSatellites";
+            this.lblTechSatellites.Size = new System.Drawing.Size(14, 13);
+            this.lblTechSatellites.TabIndex = 3;
+            this.lblTechSatellites.Text = "0";
+            // 
+            // lblTechIptvs
+            // 
+            this.lblTechIptvs.AutoSize = true;
+            this.lblTechIptvs.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.lblTechIptvs.Location = new System.Drawing.Point(17, 76);
+            this.lblTechIptvs.Name = "lblTechIptvs";
+            this.lblTechIptvs.Size = new System.Drawing.Size(14, 13);
+            this.lblTechIptvs.TabIndex = 2;
+            this.lblTechIptvs.Text = "0";
+            // 
+            // lblTechModems
+            // 
+            this.lblTechModems.AutoSize = true;
+            this.lblTechModems.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.lblTechModems.Location = new System.Drawing.Point(17, 52);
+            this.lblTechModems.Name = "lblTechModems";
+            this.lblTechModems.Size = new System.Drawing.Size(14, 13);
+            this.lblTechModems.TabIndex = 1;
+            this.lblTechModems.Text = "0";
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(4, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(119, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "For the past 7 days you have worked on:";
+            // 
+            // pnlSupervisor
+            // 
+            this.pnlSupervisor.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pnlSupervisor.Controls.Add(this.label5);
+            this.pnlSupervisor.Controls.Add(this.label6);
+            this.pnlSupervisor.Controls.Add(this.label7);
+            this.pnlSupervisor.Controls.Add(this.lblSuperSatellites);
+            this.pnlSupervisor.Controls.Add(this.lblSuperIptvs);
+            this.pnlSupervisor.Controls.Add(this.lblSuperModems);
+            this.pnlSupervisor.Controls.Add(this.label11);
+            this.pnlSupervisor.Location = new System.Drawing.Point(12, 161);
+            this.pnlSupervisor.Name = "pnlSupervisor";
+            this.pnlSupervisor.Size = new System.Drawing.Size(130, 127);
+            this.pnlSupervisor.TabIndex = 1;
+            this.pnlSupervisor.Visible = false;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(52, 100);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(49, 13);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Satellites";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(52, 76);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(41, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "IP TV\'s";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(52, 52);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(47, 13);
+            this.label7.TabIndex = 4;
+            this.label7.Text = "Modems";
+            // 
+            // lblSuperSatellites
+            // 
+            this.lblSuperSatellites.AutoSize = true;
+            this.lblSuperSatellites.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.lblSuperSatellites.Location = new System.Drawing.Point(17, 100);
+            this.lblSuperSatellites.Name = "lblSuperSatellites";
+            this.lblSuperSatellites.Size = new System.Drawing.Size(14, 13);
+            this.lblSuperSatellites.TabIndex = 3;
+            this.lblSuperSatellites.Text = "0";
+            // 
+            // lblSuperIptvs
+            // 
+            this.lblSuperIptvs.AutoSize = true;
+            this.lblSuperIptvs.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.lblSuperIptvs.Location = new System.Drawing.Point(17, 76);
+            this.lblSuperIptvs.Name = "lblSuperIptvs";
+            this.lblSuperIptvs.Size = new System.Drawing.Size(14, 13);
+            this.lblSuperIptvs.TabIndex = 2;
+            this.lblSuperIptvs.Text = "0";
+            // 
+            // lblSuperModems
+            // 
+            this.lblSuperModems.AutoSize = true;
+            this.lblSuperModems.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.lblSuperModems.Location = new System.Drawing.Point(17, 52);
+            this.lblSuperModems.Name = "lblSuperModems";
+            this.lblSuperModems.Size = new System.Drawing.Size(14, 13);
+            this.lblSuperModems.TabIndex = 1;
+            this.lblSuperModems.Text = "0";
+            // 
+            // label11
+            // 
+            this.label11.Location = new System.Drawing.Point(4, 10);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(119, 31);
+            this.label11.TabIndex = 0;
+            this.label11.Text = "For the past 7 days the workload is:";
+            // 
+            // btnSetBackColor
+            // 
+            this.btnSetBackColor.Location = new System.Drawing.Point(13, 338);
+            this.btnSetBackColor.Name = "btnSetBackColor";
+            this.btnSetBackColor.Size = new System.Drawing.Size(129, 26);
+            this.btnSetBackColor.TabIndex = 2;
+            this.btnSetBackColor.Text = "Select Back Color";
+            this.btnSetBackColor.UseVisualStyleBackColor = true;
+            this.btnSetBackColor.Click += new System.EventHandler(this.btnSetBackColor_Click);
+            // 
+            // btnSetForeColor
+            // 
+            this.btnSetForeColor.Location = new System.Drawing.Point(12, 370);
+            this.btnSetForeColor.Name = "btnSetForeColor";
+            this.btnSetForeColor.Size = new System.Drawing.Size(129, 24);
+            this.btnSetForeColor.TabIndex = 3;
+            this.btnSetForeColor.Text = "Select Fore Color";
+            this.btnSetForeColor.UseVisualStyleBackColor = true;
+            this.btnSetForeColor.Click += new System.EventHandler(this.btnSetForeColor_Click);
+            // 
+            // frmHome
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(154, 469);
+            this.Controls.Add(this.btnSetForeColor);
+            this.Controls.Add(this.btnSetBackColor);
+            this.Controls.Add(this.pnlSupervisor);
+            this.Controls.Add(this.pnlTechnician);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frmHome";
+            this.Text = "Home";
+            this.Load += new System.EventHandler(this.frmHome_Load);
+            this.pnlTechnician.ResumeLayout(false);
+            this.pnlTechnician.PerformLayout();
+            this.pnlSupervisor.ResumeLayout(false);
+            this.pnlSupervisor.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlTechnician;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblTechModems;
+        private System.Windows.Forms.Label lblTechSatellites;
+        private System.Windows.Forms.Label lblTechIptvs;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Panel pnlSupervisor;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label lblSuperSatellites;
+        private System.Windows.Forms.Label lblSuperIptvs;
+        private System.Windows.Forms.Label lblSuperModems;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.Button btnSetBackColor;
+        private System.Windows.Forms.Button btnSetForeColor;
+
+    }
+}
---
+++ OTE/frmHome.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OTE
+{
+    public partial class frmHome : Form
+    {
+        public frmHome()
+        {
+            InitializeComponent();
+        }
+
+        #region Events
+
+        private void frmHome_Load(object sender, EventArgs e)
+        {
+            foreach (var roleId in ((frmMain)this.MdiParent).Roles)
+            {
+                if ((Enums.UserRoles)Enum.Parse(typeof(Enums.UserRoles), roleId) == Enums.UserRoles.Technician)
+                {
+                    LoadTechnicianStats();
+                    pnlTechnician.Visible = true;
+                }
+
+                if ((Enums.UserRoles)Enum.Parse(typeof(Enums.UserRoles), roleId) == Enums.UserRoles.Supervisor)
+                {
+                    LoadSupervisorStats();
+                    pnlSupervisor.Visible = true;
+                    if (!pnlTechnician.Visible)
+                        pnlSupervisor.Location = new Point(12, 12);
+                }
+            }
+        }
+
+        private void btnSetBackColor_Click(object sender, EventArgs e)
+        {
+            if (colorDialog1.ShowDialog() == DialogResult.OK)
+                this.BackColor = colorDialog1.Color;
+        }
+
+        private void btnSetForeColor_Click(object sender, EventArgs e)
+        {
+            if (colorDialog1.ShowDialog() == DialogResult.OK)
+                this.ForeColor = colorDialog1.Color;
+        }
+
+        #endregion
+
+        #region Methods
+
+        private void LoadTechnicianStats()
+        {
+            DataTable statsDT = GetTechnicianStatsFromDB();
+            if (statsDT.Rows.Count > 0)
+            {
+                lblTechModems.Text = statsDT.Rows[0]["TotalModems"].ToString();
+                lblTechIptvs.Text = statsDT.Rows[0]["TotalIPTVs"].ToString();
+                lblTechSatellites.Text = statsDT.Rows[0]["TotalSatellites"].ToString();
+            }
+        }
+
+        private void LoadSupervisorStats()
+        {
+            DataTable statsDT = GetSupervisorStatsFromDB();
+            if (statsDT.Rows.Count > 0)
+            {
+                lblSuperModems.Text = statsDT.Rows[0]["TotalModems"].ToString();
+                lblSuperIptvs.Text = statsDT.Rows[0]["TotalIPTVs"].ToString();
+                lblSuperSatellites.Text = statsDT.Rows[0]["TotalSatellites"].ToString();
+            }
+        }
+
+        private DataTable GetTechnicianStatsFromDB()
+        {
+            DataTable dt = new DataTable();
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["Main"].ConnectionString))
+                {
+                    using (SqlCommand cmd = new SqlCommand("[dbo].[GetWorkloadStats]", conn))
+                    {
+                        cmd.CommandType = CommandType.StoredProcedure;
+                        cmd.Parameters.AddWithValue("@userId", ((frmMain)this.MdiParent).UserId);
+                        cmd.Parameters.AddWithValue("@hmeromhniaElenxouApo", DateTime.Today.AddDays(-7));
+                        cmd.Parameters.AddWithValue("@hmeromhniaElenxouEos", DateTime.Today);
+
+                        using (SqlDataAdapter da = new SqlDataAdapter(cmd))
+                            da.Fill(dt);
+                    }
+                }
+
+                return dt;
+            }
+            catch (Exception ex)
+            {
+                string error = ex.Message; //sto message property einai apothhkeymenh h timh toy error
+                throw;
+            }
+        }
+
+        private DataTable GetSupervisorStatsFromDB()
+        {
+            DataTable dt = new DataTable();
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["Main"].ConnectionString))
+                {
+                    using (SqlCommand cmd = new SqlCommand("[dbo].[GetWorkloadStats]", conn))
+                    {
+                        cmd.CommandType = CommandType.StoredProcedure;
+                        cmd.Parameters.AddWithValue("@userId", string.Empty);
+                        cmd.Parameters.AddWithValue("@hmeromhniaElenxouApo", DateTime.Today.AddDays(-7));
+                        cmd.Parameters.AddWithValue("@hmeromhniaElenxouEos", DateTime.Today);
+
+                        using (SqlDataAdapter da = new SqlDataAdapter(cmd))
+                            da.Fill(dt);
+                    }
+                }
+
+                return dt;
+            }
+            catch (Exception ex)
+            {
+                string error = ex.Message; //sto message property einai apothhkeymenh h timh toy error
+                throw;
+            }
+        }
+
+
+        #endregion
+
+    }
+}
